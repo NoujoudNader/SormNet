d3ff81c4f9fe60130e32ee5039a27732c6f526cf
Stefanos <sgiaremi@physics.auth.gr>
2025-01-31 11:45:53 +0200

    Re-arange branches to folders

---
+++ Ian_only/loader/dataloader_hurricane.py
@@ -0,0 +1,333 @@
+import torch
+import numpy as np
+import pandas as pd
+import os
+import math
+
+from torch_geometric.data import InMemoryDataset, Data
+from sklearn.preprocessing import StandardScaler, RobustScaler,MinMaxScaler
+
+from shutil import copyfile
+
+
+
+def haversine(lat1, lon1, lat2, lon2):
+    '''
+    Calculate distance using the Haversine Formula
+    '''
+    R = 6371000  # radius of Earth in meters
+    phi_1 = math.radians(lat1)
+    phi_2 = math.radians(lat2)
+
+    delta_phi = math.radians(lat2 - lat1)
+    delta_lambda = math.radians(lon2 - lon1)
+
+    a = math.sin(delta_phi / 2.0) ** 2 + math.cos(phi_1) * math.cos(phi_2) * math.sin(delta_lambda / 2.0) ** 2
+    
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    meters = R * c  # output distance in meters
+    km = meters / 1000.0  # output distance in kilometers
+    
+    return km
+
+def get_distance(df, station_df): # NN: save time by filling only lower triangle of matrix, upper triangle=lower triangle
+    # Ids=df['station_id'].unique() 
+    # station_df=create_stationDf(df,Ids, 'offset')
+    Ids_new=station_df.columns
+
+    # dist_arr=[]
+    dist_arr=np.zeros([len(Ids_new), len(Ids_new)])
+
+    # Loop through each row of the station data DataFrame
+    for i in range(len(Ids_new)):
+        # dist_i = []
+        station_i=df[df['station_id']==Ids_new[i]]
+        station_i.reset_index(drop=True,inplace=True)
+        lat1=station_i['x'][0]
+        lng1=station_i['y'][0]
+
+        # for j in range(len(Ids_new)):
+        for j in range(i+1):
+            if j > len(Ids_new):
+                break
+            station_j=df[df['station_id']==Ids_new[j]]
+            station_j.reset_index(drop=True,inplace=True)
+            #print(station)
+            lat2=station_j['x'][0]
+            
+            lng2=station_j['y'][0]
+            
+            dist_arr[i,j]=haversine(lat1, lng1, lat2, lng2)
+            # dist= haversine(lat1, lng1, lat2, lng2)
+            # dist_i.append(dist)
+            
+        # dist_arr.append(dist_i)
+
+    # Convert dist arrays to numpy arrays
+    # dist_arr = np.array(dist_arr)
+
+    # https://stackoverflow.com/questions/16444930/copy-upper-triangle-to-lower-triangle-in-a-python-matrix/58806735#58806735
+    dist_arr = dist_arr + dist_arr.T - np.diag(np.diag(dist_arr)) 
+
+    return dist_arr
+
+
+def get_correlation(df, station_df): # NN: save time by filling only lower triangle of matrix, upper triangle=lower triangle
+    # Ids=df['station_id'].unique()
+    # station_df=create_stationDf(df,Ids, 'offset')
+    Ids_new=station_df.columns
+
+    # correlation_mat=[]
+    correlation_mat=np.zeros([len(Ids_new), len(Ids_new)])
+    # Loop through each row of the station data DataFrame
+    for i in range(len(Ids_new)):
+        # corr_i = []
+        station_i=df[df['station_id']==Ids_new[i]]
+        station_i.reset_index(drop=True,inplace=True)
+
+        # for j in range(len(Ids_new)):
+        for j in range(i+1):
+            if j > len(Ids_new):
+                break
+
+            station_j=df[df['station_id']==Ids_new[j]]
+            station_j.reset_index(drop=True,inplace=True)
+            correlation_mat[i,j] = station_i['observed_data'].corr(station_j['observed_data'])
+            # corr = station_i['observed_data'].corr(station_j['observed_data'])
+            # corr_i.append(corr)
+            
+        # correlation_mat.append(corr_i)
+
+    # https://stackoverflow.com/questions/16444930/copy-upper-triangle-to-lower-triangle-in-a-python-matrix/58806735#58806735
+    correlation_mat = correlation_mat + correlation_mat.T - np.diag(np.diag(correlation_mat)) 
+
+
+    # Convert dist arrays to numpy arrays
+    # correlation_mat = np.array(correlation_mat)
+    return correlation_mat
+
+def create_adjancency_matrix(W,Corr, W_mask=1000, Corr_mask=0.75):
+    """
+    Create an adjacency matrix based on threshold conditions.
+
+    Parameters:
+    - W (numpy.ndarray): A distance matrix in km.
+    - Corr (numpy.ndarray): A correlation matrix.
+    - W_mask (float): The threshold for the distance matrix W.
+    - Corr_mask (float): The threshold for the correlations.
+
+    Returns:
+    - numpy.ndarray: An adjacency matrix with the correlation values as weights
+                     where both weight and correlation conditions are satisfied.
+    """
+    # Create a mask where both conditions are True
+    mask = (W < W_mask) & (np.abs(Corr) > Corr_mask)
+
+    # Create an adjacency matrix with elements from Corr where mask is True, otherwise 0
+    adjacency_matrix = np.where(mask, Corr, 0)
+
+    return adjacency_matrix
+
+def create_stationDf(data, Ids, attribute):
+    """
+    Create a dataframe where each column is a station.
+
+    Parameters:
+    - data (dataframe): The original dataframe of hurricane.
+    - Ids: stations Ids.
+    - attribute: Could be one of these: 'offset', 'observed', 'forecasted' .
+
+    Returns:
+    - dataframe: A dataframe containing the 'attribute' data for all stations, 
+                    where each station is a column.
+    """
+    
+    # df_station = pd.DataFrame()
+    df_station = {}
+
+
+    for id in Ids:
+        sequence=data[data['station_id']==id][attribute] 
+        sequence.reset_index(drop=True,inplace=True)
+        #print(sequence.shape)
+        df_station[id]=sequence
+
+    df_station = pd.DataFrame(df_station)
+    df_station=df_station.dropna(axis=1)
+
+    
+
+    return df_station
+
+def sliding_window(station_df, config, sliding_step):
+    n_window = config['N_PRED'] + config['N_HIST'] # full window length
+
+    # config['N_Windows']=int(len(station_df)/n_window)  # Works only for non-overlapping windows
+    config['N_Windows'] = int((len(station_df) - n_window)/sliding_step)+1
+
+    x = np.ones((station_df.shape[1], config['N_HIST']*config['N_Windows']))*np.nan
+    y = np.ones((station_df.shape[1], config['N_PRED']*config['N_Windows']))*np.nan
+
+
+    # full_window = np.ones((station_df.shape[1], n_window*config['N_Windows']))*np.nan
+
+    # print(n_window)
+    # print(sliding_step)
+    # print(config['N_Windows'])
+
+    # print(full_window.shape[1])
+
+
+    for count, i in enumerate(range(0, len(station_df), sliding_step)):
+
+        sta=i
+        end = sta + n_window
+        
+        if end > len(station_df):
+            break
+
+        data=np.array(station_df.iloc[sta:end,:].values)
+        data=np.swapaxes(data, 0, 1) #data is the offset of all stations
+
+        x[:,count*config['N_HIST']:count*config['N_HIST']+config['N_HIST']] = data[:, :config['N_HIST']]
+        y[:,count*config['N_PRED']:count*config['N_PRED']+config['N_PRED']] = data[:, config['N_HIST']::]
+
+    return x, y 
+
+
+def prepare_gnn_data(df, config, W_mask=1000, Corr_mask=0.7):
+    """
+    Prepare dataset for GNN 
+
+    Parameters:
+    - df (dataframe): The original dataframe of hurricane.
+    - config: config object.
+    """ 
+    Ids=df['station_id'].unique()   
+    station_df=create_stationDf(df,Ids, 'offset')
+    config['N_NODE'] = station_df.shape[1]
+
+    
+
+    # Calculate adjacency matrix - fixed for all timesteps
+    W=get_distance(df, station_df)
+    Corr=get_correlation(df, station_df)
+    adj_matrix=create_adjancency_matrix(W,Corr, W_mask, Corr_mask)
+    
+    
+
+    _,n_node = W.shape
+    n_window = config['N_PRED'] + config['N_HIST'] # full window taken per time t
+
+    # config['N_Windows']=int(len(station_df)/n_window) # Correct only if windows are non-overlapping
+    # config['N_SLOT']=24 #hourly data measurment
+
+    # edge_index: 2xnum_edges
+    # edge_index = torch.zeros((2, n_node**2), dtype=torch.long)
+    # create an edge_attr matrix with our weights  (num_edges x 1) --> our edge features are dim 1
+    edge_attr = torch.zeros((n_node**2, 1))
+    edge_index = torch.nonzero(torch.from_numpy(adj_matrix)).t()
+    mask = edge_index[0] != edge_index[1] #mask to remove self loop edges
+    filtered_edge_index = edge_index[:, mask] #remove self loop edges
+    edge_index=filtered_edge_index
+
+    edge_attr = adj_matrix[filtered_edge_index[0], filtered_edge_index[1]] #get edges attributes 
+    # edge_attr = edge_attr.resize_(edge_attr.shape[0], 1)
+
+    # Split stations_df based on config["SPLITS"]
+    station_df_train, station_df_val, station_df_test = get_splits_hurricanes(station_df, config['SPLITS'])
+
+
+    x_train, y_train = sliding_window(station_df_train, config, 1)
+    x_val, y_val = sliding_window(station_df_val, config, 1)
+    x_test, y_test = sliding_window(station_df_test, config, config['N_PRED'])
+    # print(x)
+
+
+    # Scale data
+    x_scaler = MinMaxScaler().fit(x_train.reshape(-1,1))
+    x_train = x_scaler.transform(x_train.reshape(-1,1)).reshape(x_train.shape[0], x_train.shape[1])
+    x_val = x_scaler.transform(x_val.reshape(-1,1)).reshape(x_val.shape[0], x_val.shape[1])
+    x_test = x_scaler.transform(x_test.reshape(-1,1)).reshape(x_test.shape[0], x_test.shape[1])
+
+    y_scaler = MinMaxScaler().fit(y_train.reshape(-1,1))
+    y_train = y_scaler.transform(y_train.reshape(-1,1)).reshape(y_train.shape[0], y_train.shape[1])
+    y_val = y_scaler.transform(y_val.reshape(-1,1)).reshape(y_val.shape[0], y_val.shape[1])
+    y_test = y_scaler.transform(y_test.reshape(-1,1)).reshape(y_test.shape[0], y_test.shape[1])
+
+
+    train_gnn = create_temporal_graph(n_node, edge_index, edge_attr, config, len(station_df_train), x_train, y_train)
+    val_gnn = create_temporal_graph(n_node, edge_index, edge_attr, config, len(station_df_val), x_val, y_val)
+    test_gnn = create_temporal_graph(n_node, edge_index, edge_attr, config, len(station_df_test), x_test, y_test)
+
+    return train_gnn, val_gnn, test_gnn, y_scaler
+
+    # torch.save((data, slices, n_node, mean, std_dev), self.processed_paths[0])
+
+
+def create_temporal_graph(n_node, edge_index, edge_attr, config, timesteps, x, y):
+    
+    n_window = config['N_PRED'] + config['N_HIST']
+    sequences = []
+
+    # T x F x N
+    for i in range(timesteps):
+    
+        # for each time point construct a different graph with data object
+        # Docs here: https://pytorch-geometric.readthedocs.io/en/latest/modules/data.html#torch_geometric.data.Data
+        g = Data()
+        g.__num_nodes__ = n_node
+
+        g.edge_index = edge_index
+        g.edge_attr  = edge_attr
+
+        # Parse windowed data arrays
+        # sta=i
+        # end = sta + n_window
+        
+        # if end > timesteps:
+        #     break
+        # [21, 228]
+        # data=np.array(station_df.iloc[sta:end,:].values)
+        # full_window = np.swapaxes(data, 0, 1) #data is the offset of station
+                    
+        # scaler = MinMaxScaler() # Create a scaler object NN: scale before all the data in station_df
+        # full_window = scaler.fit_transform(full_window)    # Fit and transform the data
+        # full_window = torch.tensor(scaled_data_x_np)     # Convert the scaled data back to tensor
+
+        # g.x = torch.FloatTensor(full_window[:, 0:config['N_HIST']]) #input first n past points
+        # g.y = torch.FloatTensor(full_window[:, config['N_HIST']::]) #output predicted after n points
+
+        x_start = i*config['N_HIST']
+        x_end = i*config['N_HIST']+config['N_HIST']
+        y_start = i*config['N_PRED']
+        y_end = i*config['N_PRED']+config['N_PRED']
+
+        if (x_end > x.shape[1]) or (y_end > y.shape[1]):
+            break
+
+        g.x = torch.FloatTensor(x[:,x_start:x_end]) #input first n past points
+        g.y = torch.FloatTensor(y[:,y_start:y_end]) #output predicted after n points
+        sequences += [g]
+
+    # Make the actual dataset
+    my_data= sequences
+    return my_data
+
+def get_splits_hurricanes(dataset, splits):
+    
+    """
+    Given the data, split it into sequential subsets of train, val, and test as given by splits
+    :param dataset: Dataset object to split
+    :param splits: [train, val, test] ratios (must be lower than 1 and sum of total =1)
+    """
+    split_train, split_val, _ = splits
+    n_total=len(dataset)
+    i = int(n_total*split_train)
+    j = int(n_total*split_val)
+    train = dataset[:i]
+    val = dataset[i:i+j]
+    test = dataset[i+j:]
+
+    return train, val, test
---
+++ Ian_only/models/trainer.py
@@ -0,0 +1,203 @@
+import torch
+import torch.optim as optim
+from tqdm import tqdm
+import time
+import os
+import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
+
+from models.st_gat import ST_GAT
+from models.st_gcn import ST_GCN
+
+from utils.math_utils import *
+from torch.utils.tensorboard import SummaryWriter
+
+# Make a tensorboard writer
+writer = SummaryWriter()
+
+@torch.no_grad()
+def eval(model, device, dataloader, type=''):
+    """
+    Evaluation function to evaluate model on data
+    :param model Model to evaluate
+    :param device Device to evaluate on
+    :param dataloader Data loader
+    :param type Name of evaluation type, e.g. Train/Val/Test
+    """
+    model.eval()
+    model.to(device)
+
+    mae = 0
+    rmse = 0
+    mape = 0
+    n = 0
+
+    # Evaluate model on all data
+    for i, batch in enumerate(dataloader):
+        batch = batch.to(device)
+        if batch.x.shape[0] == 1:
+            pass
+        else:
+            # plt.figure()
+            with torch.no_grad():
+                pred = model(batch, device)
+               
+            truth = batch.y.view(pred.shape)
+            # print("Truth: ",truth.shape)
+            # print("Pred: ", pred.shape)
+            # plt.plot(truth)
+            # plt.show()
+
+
+            if i == 0:
+                y_pred = torch.zeros(len(dataloader), pred.shape[0], pred.shape[1])
+                y_truth = torch.zeros(len(dataloader), pred.shape[0], pred.shape[1])
+            # truth = un_z_score(truth, dataloader.dataset.mean, dataloader.dataset.std_dev)
+            # pred = un_z_score(pred, dataloader.dataset.mean, dataloader.dataset.std_dev)
+            y_pred[i, :pred.shape[0], :] = pred
+            y_truth[i, :pred.shape[0], :] = truth
+
+            rmse += RMSE(truth, pred)
+            mae += MAE(truth, pred)
+            mape += MAPE(truth, pred)
+            n += 1
+    rmse, mae, mape = rmse / n, mae / n, mape / n
+
+    print(f'{type}, MAE: {mae}, RMSE: {rmse}')
+
+    #get the average score for each metric in each batch
+    return rmse, mae, mape, y_pred, y_truth
+
+def train(model, device, dataloader, optimizer, loss_fn, epoch):
+    """
+    Evaluation function to evaluate model on data
+    :param model Model to evaluate
+    :param device Device to evaluate on
+    :param dataloader Data loader
+    :param optimizer Optimizer to use
+    :param loss_fn Loss function
+    :param epoch Current epoch
+    """
+    model.train()
+    for _, batch in enumerate(tqdm(dataloader, desc=f"Epoch {epoch}")):
+        batch = batch.to(device)
+        optimizer.zero_grad()
+        y_pred = torch.squeeze(model(batch, device))
+        loss = loss_fn()(y_pred.float(), torch.squeeze(batch.y).float())
+        writer.add_scalar("Loss/train", loss, epoch)
+        loss.backward()
+        optimizer.step()
+
+    return loss
+
+def model_train(train_dataloader, val_dataloader, config, device):
+    """
+    Train the ST-GAT model. Evaluate on validation dataset as you go.
+    :param train_dataloader Data loader of training dataset
+    :param val_dataloader Dataloader of val dataset
+    :param config configuration to use
+    :param device Device to evaluate on
+    """
+
+    # Make the model. Each datapoint in the graph is 228x12: N x F (N = # nodes, F = time window)
+    # model = ST_GAT(in_channels=config['N_HIST'], out_channels=config['N_PRED'], n_nodes=config['N_NODE'], dropout=config['DROPOUT'])
+    
+    model = ST_GAT(in_channels=config['N_HIST'], out_channels=config['N_PRED'], n_nodes=config['N_NODE'], dropout=config['DROPOUT'])
+    optimizer = optim.Adam(model.parameters(), lr=config['INITIAL_LR'], weight_decay=config['WEIGHT_DECAY'])
+    loss_fn = torch.nn.MSELoss
+
+    model.to(device)
+
+    # For every epoch, train the model on training dataset. Evaluate model on validation dataset
+    for epoch in range(config['EPOCHS']):
+        loss = train(model, device, train_dataloader, optimizer, loss_fn, epoch)
+        print(f"Loss: {loss:.5f}")
+        if epoch % 5 == 0:
+            train_mae, train_rmse, train_mape, _, _ = eval(model, device, train_dataloader, 'Train')
+            val_mae, val_rmse, val_mape, _, _ = eval(model, device, val_dataloader, 'Valid')
+            writer.add_scalar(f"MAE/train", train_mae, epoch)
+            writer.add_scalar(f"RMSE/train", train_rmse, epoch)
+            writer.add_scalar(f"MAPE/train", train_mape, epoch)
+            writer.add_scalar(f"MAE/val", val_mae, epoch)
+            writer.add_scalar(f"RMSE/val", val_rmse, epoch)
+            writer.add_scalar(f"MAPE/val", val_mape, epoch)
+
+    writer.flush()
+    # Save the model
+    timestr = time.strftime("%m-%d-%H%M%S")
+    torch.save({
+            "epoch": epoch,
+            "model_state_dict": model.state_dict(),
+            "optimizer_state_dict": optimizer.state_dict(),
+            "loss": loss,
+            }, os.path.join(config["CHECKPOINT_DIR"], f"model_{timestr}.pt"))
+
+    return model
+
+def model_test(model, test_dataloader, device, node, config):
+    """
+    Test the ST-GAT model
+    :param test_dataloader Data loader of test dataset
+    :param device Device to evaluate on
+    """
+    _, _, _, y_pred, y_truth = eval(model, device, test_dataloader, 'Test')
+    print("Pred:",y_pred.shape)
+    print("Truth:",y_truth.shape)
+    plot_prediction(test_dataloader, y_pred, y_truth, node, config)
+
+
+def plot_prediction(test_dataloader, y_pred, y_truth, node, config):
+    # Calculate the truth
+    s = y_truth.shape
+    print("shape:",s[1])
+    num=int(s[1]* s[-1])
+    num2=config['N_NODE']*s[-1] 
+    y_truth = y_truth.reshape(s[0], int(num/num2), config['N_NODE'], s[-1])
+    print("shape after:",y_truth.shape)
+    
+    print(y_truth[:, :, node, :])
+
+    # just get the first prediction out for the nth node
+    y_truth = y_truth[:, :, node, 0]
+    # Flatten to get the predictions for entire test dataset
+    y_truth = torch.flatten(y_truth)
+    # print(y_truth)
+
+    # day0_truth = y_truth[:config['N_SLOT']]
+
+
+    # Calculate the predicted
+    s = y_pred.shape
+    y_pred = y_pred.reshape(s[0], int(num/num2) ,config['N_NODE'], s[-1])
+    print("shape after:",y_pred.shape)
+    print(y_pred[:, :, node, :])
+    # just get the first prediction out for the nth node
+    y_pred = y_pred[:, :, node, 0]
+    # Flatten to get the predictions for entire test dataset
+    y_pred = torch.flatten(y_pred)
+    print(y_pred)
+
+    print("R\u00b2 = {:.2f}".format(r2_score(y_truth, y_pred)))
+    t = [t*60 for t in range(0, len(y_pred))]
+    plt.plot(t, y_pred, marker='o',label='ST-GAT')
+    plt.plot(t, y_truth, marker='s',label='truth')
+    # plt.xlabel('Time (mins.)')
+    plt.ylabel('Offset (ft.)')
+    plt.title('Predictions over time')
+    plt.legend()
+    # plt.savefig('predicted_times.png')
+    plt.show()
+
+
+
+def load_from_checkpoint(checkpoint_path, config):
+    """
+    Load a model from the checkpoint
+    :param checkpoint_path Path to checkpoint
+    :param config Configuration to load model with
+    """
+    model = ST_GAT(in_channels=config['N_HIST'], out_channels=config['N_PRED'], n_nodes=config['N_NODE'])
+    checkpoint = torch.load(checkpoint_path, map_location=torch.device('cpu'))
+    model.load_state_dict(checkpoint['model_state_dict'])
+
+    return model
